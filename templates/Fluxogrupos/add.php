9746945fac1db3ae600503b6014a87b9e966da30
emanuel <emanuelvanobre@gmail.com>
2021-09-10 14:21:01 -0300

    Feito

--- templates/Fluxogrupos/add.php
+++ templates/Fluxogrupos/add.php
@@ -5,34 +5,19 @@
  */
 ?>
 
-<?php $this->assign('title', __('Add Fluxogrupo') ); ?>
-
-<?php
-$this->assign('breadcrumb',
-  $this->element('content/breadcrumb', [
-    'home' => true,
-    'breadcrumb' => [
-      'List Fluxogrupos' => ['action'=>'index'],
-      'Add',
-    ]
-  ])
-);
-?>
-
+<?php $this->assign('title', __('Adicionar   Fluxogrupo') ); ?>
 
 <div class="card card-primary card-outline">
   <?= $this->Form->create($fluxogrupo) ?>
   <div class="card-body">
-    <?php
-      echo $this->Form->control('grupo');
-      echo $this->Form->control('descricao');
-    ?>
+    <?= $this->Form->control('grupo', [ 'label' => 'Grupo' ]); ?>
+    <?= $this->Form->control('descricao', [ 'label' => 'Descrição' ]); ?>
   </div>
 
   <div class="card-footer d-flex">
     <div class="ml-auto">
-      <?= $this->Form->button(__('Save')) ?>
-      <?= $this->Html->link(__('Cancel'), ['action'=>'index'], ['class'=>'btn btn-default']) ?>
+      <?= $this->Form->button(__('Salvar')) ?>
+      <?= $this->Html->link(__('Cancelar'), ['action'=>'index'], ['class'=>'btn btn-default']) ?>
     </div>
   </div>
 
