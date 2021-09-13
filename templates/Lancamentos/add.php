a2bb11b14b55fb7e18e10ca283197ff3713e8f8e
emanuel <emanuelvanobre@gmail.com>
2021-09-13 16:42:42 -0300

    Stepper feitinho

--- templates/Lancamentos/add.php
+++ templates/Lancamentos/add.php
@@ -9,99 +9,104 @@
 
   <?= $this->Html->css('bs-stepper.min.css'); ?>
   <?= $this->Html->script('bs-stepper.min.js'); ?>
-
-  <div class="container">
-        <div id="stepper1" class="bs-stepper">
-          <div class="bs-stepper-header bg-light">
-            <div class="step" data-target="#test-l-1">
-              <button type="button" class="btn step-trigger">
-                <span class="bs-stepper-circle">1</span>
-                <span class="bs-stepper-label">First step</span>
-              </button>
+  <div class="container" style="width: 60%;min-width:80%">
+    <div id="stepper1" class="bs-stepper ">
+      <div class="bs-stepper-header bg-light" style="margin: 0px;">
+        <div class="step" data-target="#test-l-1">
+          <button type="button" class="btn step-trigger">
+            <span class="bs-stepper-circle">1</span>
+          </button>
+        </div>
+        <div class="line"></div>
+        <div class="step" data-target="#test-l-2">
+          <button type="button" class="btn step-trigger">
+            <span class="bs-stepper-circle">2</span>
+          </button>
+        </div>
+        <div class="line"></div>
+        <div class="step" data-target="#test-l-3">
+          <button type="button" class="btn step-trigger">
+            <span class="bs-stepper-circle">3</span>
+          </button>
+        </div>
+        <div class="line"></div>
+        <div class="step" data-target="#test-l-4">
+          <button type="button" class="btn step-trigger">
+            <span class="bs-stepper-circle">4</span>
+          </button>
+        </div>
+      </div>
+      <div class="bs-stepper-content bg-dark" style="padding: 20px;">
+        <?= $this->Form->create($lancamento) ?>
+        <div id="test-l-1" class="content bg-dark">
+          <div class="panel-body">
+            <div class="form-group">
+              <?= $this->Form->control('tipo', ['label' => 'Tipo', 'placeholder' => 'tipo'], ['class' => 'form-control']); ?>
             </div>
-            <div class="line"></div>
-            <div class="step" data-target="#test-l-2">
-              <button type="button" class="btn step-trigger">
-                <span class="bs-stepper-circle">2</span>
-                <span class="bs-stepper-label">Second step</span>
-              </button>
+            <div class="form-group">
+              <?= $this->Form->control('descricao', ['label' => 'Descrição', 'placeholder' => 'Descrição'], ['class' => 'form-control']); ?>
             </div>
-            <div class="line"></div>
-            <div class="step" data-target="#test-l-3">
-              <button type="button" class="btn step-trigger">
-                <span class="bs-stepper-circle">3</span>
-                <span class="bs-stepper-label">Third step</span>
-              </button>
-            </div>
-            <div class="line"></div>
-            <div class="step" data-target="#test-l-4">
-              <button type="button" class="btn step-trigger">
-                <span class="bs-stepper-circle">4</span>
-                <span class="bs-stepper-label">Third step</span>
-              </button>
+            <div class="form-group">
+              <?= $this->Form->control('valor', ['label' => 'Valor', 'placeholder' => 'Valor'], ['class' => 'form-control']); ?>
             </div>
           </div>
-          <div class="bs-stepper-content bg-dark">
-            <?= $this->Form->create($lancamento) ?>
-            <div id="test-l-1" class="content bg-dark">
-              <div class="panel-body">
-                <div class="form-group">
-
-                  <?= $this->Form->control('tipo', ['label' => 'Tipo', 'placeholder' => 'tipo'], ['class' => 'form-control']); ?>
-                </div>
-                <div class="form-group">
-                  <?= $this->Form->control('descricao', ['label' => 'Descrição', 'placeholder' => 'Descrição'], ['class' => 'form-control']); ?>
-                </div>
-
-                <div class="form-group">
-                  <?= $this->Form->control('valor', ['label' => 'Valor', 'placeholder' => 'Valor'], ['class' => 'form-control']); ?>
-                </div>
-              </div>
-              <div class="btn btn-primary" onclick="stepper1.next()">Next</div>
-            </div>
-            <div id="test-l-2" class="content bg-dark">
-              <div class="panel-body">
-                <div class="form-group">
-                  <?= $this->Form->control('data_emissao', ['label' => 'Data de Emissão', 'placeholder' => 'Data de Emissão'], ['class' => 'form-control']); ?>
-                </div>
-                <div class="form-group">
-                  <?= $this->Form->control('data_baixa', ['label' => 'Data de Baixa', 'placeholder' => 'Data de Baixa'], ['class' => 'form-control']); ?>
-                </div>
-                <div class="form-group">
-                  <?= $this->Form->control('data_vencimento', ['label' => 'Data de Vencimento', 'placeholder' => 'Data de Vecimento'], ['class' => 'form-control']); ?>
-                </div>
-              </div>
-              <div class="btn btn-primary" onclick="stepper1.next()">Next</div>
-              <div class="btn btn-primary" onclick="stepper1.previous()">Previous</div>
-            </div>
-            <div id="test-l-3" class="content bg-dark">
-              <div class="panel-body">
-                <div class="form-group">
-                  <?= $this->Form->control('fluxoconta_id', ['options' => $fluxocontas, 'empty' => true]); ?>
-                </div>
-                <div class="form-group">
-                  <?= $this->Form->control('dreconta_id', ['options' => $drecontas, 'empty' => true]); ?>
-                </div>
-              </div>
-              <div class="btn btn-primary" onclick="stepper1.next()">Next</div>
-              <div class="btn btn-primary" onclick="stepper1.previous()">Previous</div>
-            </div>
-            <div id="test-l-4" class="content bg-dark">
-              <div class="panel-body">
-                <div class="form-group">
-                  <?= $this->Form->control('fornecedor_id', ['options' => $fornecedores, 'empty' => true]); ?>
-                </div>
-                <div class="form-group">
-                  <?= $this->Form->control('cliente_id', ['options' => $clientes, 'empty' => true]); ?>
-                </div>
-              </div>
-              <?= $this->Form->button(__('Save', ['class' => 'btn btn-dark pull-right'])) ?>
-              <?= $this->Html->link(__('Cancel'), ['action' => 'index'], ['class' => 'btn btn-primary']) ?>
-              <div class="btn btn-primary" onclick="stepper1.previous()">Previous</div>
-            </div>
-            <?= $this->Form->end() ?>
+          <div class="d-flex justify-content-end">
+          <div class="btn btn-primary " onclick="stepper1.next()">Próximo</div>
           </div>
         </div>
+        <div id="test-l-2" class="content bg-dark">
+          <div class="panel-body">
+            <div class="form-group">
+              <?= $this->Form->control('data_emissao', ['label' => 'Data de Emissão', 'placeholder' => 'Data de Emissão'], ['class' => 'form-control']); ?>
+            </div>
+            <div class="form-group">
+              <?= $this->Form->control('data_baixa', ['label' => 'Data de Baixa', 'placeholder' => 'Data de Baixa'], ['class' => 'form-control']); ?>
+            </div>
+            <div class="form-group">
+              <?= $this->Form->control('data_vencimento', ['label' => 'Data de Vencimento', 'placeholder' => 'Data de Vecimento'], ['class' => 'form-control']); ?>
+            </div>
+          </div>
+          <div class="d-flex justify-content-between">
+            <div class="btn btn-primary" onclick="stepper1.previous()">Voltar</div>
+            <div class="btn btn-primary" onclick="stepper1.next()">Próximo</div>
+          </div>
+        </div>
+        <div id="test-l-3" class="content bg-dark">
+          <div class="panel-body">
+            <div class="form-group">
+              <?= $this->Form->control('fluxoconta_id', ['options' => $fluxocontas, 'empty' => true]); ?>
+            </div>
+            <div class="form-group">
+              <?= $this->Form->control('dreconta_id', ['options' => $drecontas, 'empty' => true]); ?>
+            </div>
+          </div>
+          <div class="d-flex justify-content-between">
+            <div class="btn btn-primary" onclick="stepper1.previous()">Voltar</div>
+            <div class="btn btn-primary" onclick="stepper1.next()">Próximo</div>
+          </div>
+        </div>
+        <div id="test-l-4" class="content bg-dark">
+          <div class="panel-body">
+            <div class="form-group">
+              <?= $this->Form->control('fornecedor_id', ['options' => $fornecedores, 'empty' => true]); ?>
+            </div>
+            <div class="form-group">
+              <?= $this->Form->control('cliente_id', ['options' => $clientes, 'empty' => true]); ?>
+            </div>
+          </div>
+          <div class="d-flex justify-content-between">
+            <div>
+              <?= $this->Html->link(__('Cancelar'), ['action' => 'index'], ['class' => 'btn btn-primary']) ?>
+            </div>
+            <div>
+              <div class="btn btn-primary" onclick="stepper1.previous()">Voltar</div>
+              <?= $this->Form->button(__('Salvar', ['class' => 'btn btn-dark pull-right']), ['confirm' => 'Quer mesmo salvar esse lançamento?']) ?>
+            </div>
+          </div>
+        </div>
+        <?= $this->Form->end() ?>
+      </div>
+    </div>
   </div>
   <script>
     var stepper1Node = document.querySelector('#stepper1')
