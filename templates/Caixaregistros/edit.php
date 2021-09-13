a2d89981b9f8ee19932f6cef11d7fb42c9fe74b0
Otacilio <otaciliofilho289@gmail.com>
2021-09-13 10:06:59 -0300

    Caixa Registro

--- templates/Caixaregistros/edit.php
+++ templates/Caixaregistros/edit.php
@@ -5,20 +5,7 @@
  */
 ?>
 
-<?php $this->assign('title', __('Edit Caixaregistro') ); ?>
-
-<?php
-$this->assign('breadcrumb',
-  $this->element('content/breadcrumb', [
-    'home' => true,
-    'breadcrumb' => [
-      'List Caixaregistros' => ['action'=>'index'],
-      'View' => ['action'=>'view', $caixaregistro->id_caixaregistro],
-      'Edit',
-    ]
-  ])
-);
-?>
+<?php $this->assign('title', __('Editar Caixa Registro') ); ?>
 
 
 <div class="card card-primary card-outline">
@@ -32,16 +19,9 @@ $this->assign('breadcrumb',
   </div>
 
   <div class="card-footer d-flex">
-    <div class="">
-      <?= $this->Form->postLink(
-          __('Delete'),
-          ['action' => 'delete', $caixaregistro->id_caixaregistro],
-          ['confirm' => __('Are you sure you want to delete # {0}?', $caixaregistro->id_caixaregistro), 'class' => 'btn btn-danger']
-      ) ?>
-    </div>
     <div class="ml-auto">
-      <?= $this->Form->button(__('Save')) ?>
-      <?= $this->Html->link(__('Cancel'), ['action'=>'index'], ['class'=>'btn btn-default']) ?>
+      <?= $this->Form->button(__('Salvar')) ?>
+      <?= $this->Html->link(__('Cancelar'), ['action'=>'index'], ['class'=>'btn btn-default']) ?>
     </div>
   </div>
 
