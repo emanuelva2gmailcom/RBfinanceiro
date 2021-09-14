7684c183e504b22ccf4b5c881ffe374ddbbcb598
Otacilio <otaciliofilho289@gmail.com>
2021-09-13 22:18:36 -0300

    sidebar

--- vendor/arodu/cakelte/templates/element/sidebar/menu.php
+++ vendor/arodu/cakelte/templates/element/sidebar/menu.php
@@ -1,5 +1,9 @@
 <li class="nav-item" style="color: #E1E7E0">
-  <a href="dashboard" class="nav-link" style="color: #E1E7E0">
+  <a href="<?= $this->Url->build([
+              'controller' => 'users',
+              'action' => 'dashboard',
+              'plugin' => 'Usermgmt',
+            ]); ?>" class="nav-link" style="color: #E1E7E0">
     <i class="nav-icon fas fa-th" style="color: #E1E7E0"></i>
     <p>
       Home
@@ -8,13 +12,14 @@
 </li>
 
 <li class="nav-item" style="color: #E1E7E0">
-  <a href="lancamentos/add" class="nav-link" style="color: #E1E7E0">
+  <a href="<?= $this->Url->build('/lancamentos/add', ['fullBase' => true]);?>" class="nav-link" style="color: #E1E7E0">
     <i class="nav-icon fas fa-th" style="color: #E1E7E0"></i>
     <p>
       Novo Lançamento
     </p>
   </a>
 </li>
+
 
 <li class="nav-item has-treeview menu-open" style="color: #E1E7E0">
   <a href="#" class="nav-link" style="color: #E1E7E0">
@@ -26,25 +31,26 @@
   </a>
   <ul class="nav nav-treeview">
     <li class="nav-item" style="color: #E1E7E0">
-      <a href="drecontas" class="nav-link" style="color: #E1E7E0; margin-left: 25px;">
+      <a href="<?= $this->Url->build('/drecontas/index', ['fullBase' => true]);?>" class="nav-link" style="color: #E1E7E0; margin-left: 25px;">
         <i class="far fa-circle nav-icon" style="color: #E1E7E0"></i>
         <p>DRE</p>
       </a>
     </li>
     <li class="nav-item" style="color: #E1E7E0">
-      <a href="fluxocontas" class="nav-link" style="color: #E1E7E0; margin-left: 25px;">
+      <a href="<?= $this->Url->build('/caixas/index', ['fullBase' => true]);?>" class="nav-link" style="color: #E1E7E0; margin-left: 25px;">
         <i class="far fa-circle nav-icon" style="color: #E1E7E0"></i>
         <p>Fluxo de Caixa</p>
       </a>
     </li>
     <li class="nav-item" style="color: #E1E7E0">
-      <a href="lancamentos" class="nav-link" style="color: #E1E7E0; margin-left: 25px;">
+      <a href="<?= $this->Url->build('/lancamentos/index', ['fullBase' => true]);?>
+                " class="nav-link" style="color: #E1E7E0; margin-left: 25px;">
         <i class="far fa-circle nav-icon" style="color: #E1E7E0"></i>
         <p>Lançamentos</p>
       </a>
     </li>
     <li class="nav-item" style="color: #E1E7E0">
-      <a href="caixaregistros" class="nav-link" style="color: #E1E7E0; margin-left: 25px;">
+      <a href="<?= $this->Url->build('/caixaregistros/index', ['fullBase' => true]);?>" class="nav-link" style="color: #E1E7E0; margin-left: 25px;">
         <i class="far fa-circle nav-icon" style="color: #E1E7E0"></i>
         <p>Caixa Gerencial</p>
       </a>
@@ -53,7 +59,11 @@
 </li>
 
 <li class="nav-item" style="color: #E1E7E0">
-  <a href="dashboard" class="nav-link" style="color: #E1E7E0">
+  <a href="<?= $this->Url->build([
+              'controller' => 'users',
+              'action' => 'dashboard',
+              'plugin' => 'Usermgmt',
+            ]); ?>" class="nav-link" style="color: #E1E7E0">
     <i class="nav-icon fas fa-th" style="color: #E1E7E0"></i>
     <p>
       Painel
