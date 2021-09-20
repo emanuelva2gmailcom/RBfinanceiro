04c905e0164c4116212fa5e905739dbe9949ba56
vinny0240 <vinnyandrade24@gmail.com>
2021-09-20 16:50:44 -0300

    Revert "Revert "Revert "."""
    This reverts commit b5a48cca17b114bfee9493c7691ff3e0e1bbc2db.

--- vendor/arodu/cakelte/templates/element/aside/main.php
+++ vendor/arodu/cakelte/templates/element/aside/main.php
@@ -1,4 +1,8 @@
-<div class="p-3">
-  <h5>Title</h5>
-  <p>Sidebar content</p>
+<div class="p-3" style="color: #E1E7E0">
+  <li class="nav-item d-sm-block" style="list-style: none;">
+    <?= $this->Html->link(__('Configurações'), ['controller' => 'users','action' => 'logout'], ['class' => 'nav-link']) ?>
+  </li>
+  <li class="nav-item d-sm-block" style="list-style: none;">
+    <?= $this->Html->link(__('Sair'), ['controller' => 'users','action' => 'logout'], ['class' => 'nav-link']) ?>
+  </li>
 </div>
