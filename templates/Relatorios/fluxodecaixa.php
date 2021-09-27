7e226a4e78a4b15e515d46774dfef5dceb650cf6
emanuel <emanuelvanobre@gmail.com>
2021-09-27 04:51:53 -0300

    $obj construido

--- templates/Relatorios/fluxodecaixa.php
+++ templates/Relatorios/fluxodecaixa.php
@@ -1,15 +1,27 @@
 <?php if($show == true){ ?>
 <style>
-.table td{
+tr > td{
     min-width: 100px;
 }
+
+.text-nw{
+    padding: 0px;
+    margin: 0px;
+    white-space: nowrap;
+    overflow: auto;
+}
+
+.mwtd{
+    min-width: 150px;
+}
+
 </style>
-<div class="card-body table-responsive">
-    <table class="table table-sm table-light rounded">
+<div class="container">
+    <table class="table table-sm table-bordered table-dark rounded table-responsive w-100">
         <thead class="bg-primary">
             <tr>
-                <th scope="col"></th>
-                <?php foreach($datas as $data): ?>
+                <th scope="col" class="mwtd"></th>
+                <?php foreach($obj['header'] as $data): ?>
                     <th scope="col"><?= $data ?></th>
                 <?php endforeach; ?>
                 <th scope="col">Total</th>
@@ -18,11 +30,11 @@
         </thead>
         <tbody class="text-success">
         <?php 
-            foreach($valores as $valor):
-                if(in_array($valor[0], $entradas)){
+            foreach($obj['rows']['td'] as $valor):
+                if(in_array($valor[0], $obj['rows']['th']['entradas'])){
                 ?>
                     <tr>
-                        <th scope="row" class="bg-lightblue"><?= $valor[0] ?></th>
+                        <th scope="row" class="bg-lightblue"><p class="text-nw"><?= $valor[0] ?></p></th>
                         <?php for($i = 1; $i < count($valor); $i++): ?>
                             <td><?= $valor[$i] ?></td>
                         <?php endfor; ?>
@@ -34,19 +46,19 @@
         </tbody>
         <thead class="bg-primary">
             <tr>
-                <th scope="row">Entradas</th>
-                <?php foreach($totale as $t): ?>
+                <th scope="row" class="mwtd"><p class="text-nw">Entradas</p></th>
+                <?php foreach($obj['total']['entradas'] as $t): ?>
                     <td><?= $t ?></td>
                 <?php endforeach; ?>
             </tr>
         </thead>
         <tbody class="text-danger">
         <?php 
-            foreach($valores as $valor):
-                if(in_array($valor[0], $saidas)){
+            foreach($obj['rows']['td'] as $valor):
+                if(in_array($valor[0], $obj['rows']['th']['saidas'])){
                 ?>
                     <tr>
-                        <th scope="row" class="bg-lightblue"><?= $valor[0] ?></th>
+                        <th scope="row" class="bg-lightblue"><p class="text-nw"><?= $valor[0] ?></p></th>
                         <?php for($i = 1; $i < count($valor); $i++): ?>
                             <td><?= $valor[$i] ?></td>
                         <?php endfor; ?>
@@ -58,36 +70,36 @@
         </tbody>
         <thead class="bg-primary">
             <tr>
-                <th scope="row">Saídas</th>
-                <?php foreach($totals as $t): ?>
+                <th scope="row" class="mwtd">Saídas</th>
+                <?php foreach($obj['total']['entradas'] as $t): ?>
                     <td><?= $t ?></td>
                 <?php endforeach; ?>
             </tr>
         </thead>
-        <thead class="thead-light">
+        <thead class="thead-dark">
             <tr>
                 <th scope="row">ENTRADAS - SAIDAS</th>
-                <?php foreach($entradas_saidas as $t): ?>
+                <?php foreach($obj['total']['entradas-saidas'] as $t): ?>
                     <td><?= $t ?></td>
                 <?php endforeach; ?>
             </tr>
             <tr>
                 <th scope="row">INICIAL</th>
-                <?php foreach($inicial as $t): ?>
+                <?php foreach($obj['total']['inicial'] as $t): ?>
                     <td><?= $t ?></td>
                 <?php endforeach; ?>
             </tr>
             <tr>
                 <th scope="row">FINAL</th>
-                <?php foreach($final as $t): ?>
+                <?php foreach($obj['total']['final'] as $t): ?>
                     <td><?= $t ?></td>
                 <?php endforeach; ?>
             </tr>
         </thead>
+        <div class="d-flex justify-content-start bg-light rounded-top" style="padding: 5px;">
+            <a href="/relatorios/fluxodecaixa" class="btn btn-none border border-primary text-primary">Voltar</a>
+        </div>
     </table>
-    <div class="d-flex justify-content-start bg-light rounded-bottom" style="padding: 5px;">
-        <a href="/relatorios/fluxodecaixa" class="btn btn-none border border-primary text-primary">Voltar</a>
-    </div>
 </div>
 <?php } else{?>
     <div class="container bg-dark">
--- templates/Relatorios/gerencial.php
+++ templates/Relatorios/gerencial.php
@@ -1,7 +1,16 @@
 <?php if($show == true){ ?>
-<div class="card-body table-responsive p-0">
-    <table class="table table-bordered bg-light">
-        <thead>
+<style>
+.table td{
+    min-width: 100px;
+}
+
+.table th{
+    min-width: 250px;
+}
+
+</style>
+    <table class="table table-sm table-bordered table-dark rounded table-responsive">
+        <thead class="bg-primary">
             <tr>
                 <th scope="col"></th>
                 <?php foreach($datas as $data): ?>
@@ -9,9 +18,7 @@
                 <?php endforeach; ?>
                 <th scope="col">Total</th>
             </tr>
-        <tr>
-            <th scope="row">Entradas</th>
-        </tr>
+        
         </thead>
         <tbody class="text-success">
         <?php 
@@ -19,7 +26,7 @@
                 if(in_array($valor[0], $entradas)){
                 ?>
                     <tr>
-                        <th scope="row"><?= $valor[0] ?></th>
+                        <th scope="row" class="bg-lightblue"><?= $valor[0] ?></th>
                         <?php for($i = 1; $i < count($valor); $i++): ?>
                             <td><?= $valor[$i] ?></td>
                         <?php endfor; ?>
@@ -28,12 +35,14 @@
                 }
             endforeach;
         ?>
-        
         </tbody>
-        <thead>
-        <tr>
-            <th scope="row">Saídas</th>
-        </tr>
+        <thead class="bg-primary">
+            <tr>
+                <th scope="row">Entradas</th>
+                <?php foreach($totale as $t): ?>
+                    <td><?= $t ?></td>
+                <?php endforeach; ?>
+            </tr>
         </thead>
         <tbody class="text-danger">
         <?php 
@@ -41,7 +50,7 @@
                 if(in_array($valor[0], $saidas)){
                 ?>
                     <tr>
-                        <th scope="row"><?= $valor[0] ?></th>
+                        <th scope="row" class="bg-lightblue"><?= $valor[0] ?></th>
                         <?php for($i = 1; $i < count($valor); $i++): ?>
                             <td><?= $valor[$i] ?></td>
                         <?php endfor; ?>
@@ -51,9 +60,26 @@
             endforeach;
         ?>
         </tbody>
+        <thead class="bg-primary">
+            <tr>
+                <th scope="row">Saídas</th>
+                <?php foreach($totals as $t): ?>
+                    <td><?= $t ?></td>
+                <?php endforeach; ?>
+            </tr>
+        </thead>
+        <thead class="thead-dark">
+            <tr>
+                <th scope="row">ENTRADAS - SAIDAS</th>
+                <?php foreach($entradas_saidas as $t): ?>
+                    <td><?= $t ?></td>
+                <?php endforeach; ?>
+            </tr>
+        </thead>
+        <div class="d-flex justify-content-start bg-light rounded-top" style="padding: 5px;">
+            <a href="/relatorios/fluxodecaixa" class="btn btn-none border border-primary text-primary">Voltar</a>
+        </div>
     </table>
-    <?= $this->Html->link('Voltar', ['controller' => 'Relatorios', 'action' => 'gerencial']) ?>
-</div>
 <?php } else{?>
     <div class="container bg-dark">
     <div class="panel-body">
