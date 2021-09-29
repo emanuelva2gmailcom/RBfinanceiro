adaea7097e64d8e647cf64efd4f2d584dd748f3a
vinny0240 <vinnyandrade24@gmail.com>
2021-09-29 10:27:05 -0300

    é isso ai

--- vendor/arodu/cakelte/templates/element/sidebar/menu.php
+++ vendor/arodu/cakelte/templates/element/sidebar/menu.php
@@ -5,11 +5,7 @@ $now = date('d-m-Y');
 ?>
 
 <li class="nav-item" style="color: #E1E7E0">
-  <a href="<?= $this->Url->build([
-              'controller' => 'users',
-              'action' => 'dashboard',
-              'plugin' => 'Usermgmt',
-            ]); ?>" class="nav-link" style="color: #E1E7E0">
+<a href="/relatorios/home" class="nav-link" style="color: #E1E7E0">
     <i class="nav-icon fas fa-th" style="color: #E1E7E0"></i>
     <p>
       Home
