9d2f5412d0e022540b120713afc6f2ae111f28e2
vinny0240 <vinnyandrade24@gmail.com>
2021-10-08 12:51:45 -0300

    pronto parte 2

--- templates/Fornecedores/index.php
+++ templates/Fornecedores/index.php
@@ -6,56 +6,53 @@
 ?>
 
 <?php $this->assign('title', __('Fornecedores') ); ?>
-<style>
-  .teste{
-    color: #E1E7F0;
-  }
-</style>
 
 
-<div class="card card-primary card-outline bg-dark">
-  <div class="card-header d-sm-flex">
-    <h2 class="card-title"><!-- --></h2>
+
+<div class="container-fluid d-flex align-items-center justify-content-center p-5">
+    <div class="card container card-outline bg-white" style="border: green solid 2px; border-radius: 20px;">
+        <div class="card-header d-sm-flex" style="padding-top: 50px;">
     <div class="card-toolbox">
       <?= $this->Paginator->limitControl([], null, [
             'label'=>false,
             'class' => 'form-control-sm',
+            'style' => 'color: #029BE1; border: 2px solid green;',
           ]); ?>
-      <?= $this->Html->link(__('Novo Fornecedor'), ['action' => 'add'], ['class' => 'btn btn-primary btn-sm']) ?>
+      <?= $this->Html->link(__('Novo Fornecedor'), ['action' => 'add'], ['class' => 'btn btn-info btn-sm']) ?>
     </div>
   </div>
   <!-- /.card-header -->
   <div class="card-body table-responsive p-0">
     <table class="table text-nowrap">
         <thead>
-          <tr>
-              
+        <tr style="color: green;">
+
               <th class="teste"><?= ('Nome') ?></th>
               <th class="teste"><?= ('CNPJ') ?></th>
               <th class="teste"><?= ('Responsável') ?></th>
               <th class="teste"><?= ('Endereço') ?></th>
               <th class="teste"><?= ('E-mail') ?></th>
               <th class="teste"><?= ('Telefone') ?></th>
-              
+
               <th class="teste"><?= ('Is_Pendente') ?></th>
               <th class="actions teste"><?= __('Ações') ?></th>
           </tr>
         </thead>
         <tbody>
           <?php foreach ($fornecedores as $fornecedore): ?>
-          <tr>
-            
+            <tr style="color: #029BE1;">
+
             <td><?= h($fornecedore->nome) ?></td>
             <td><?= h($fornecedore->cnpj) ?></td>
             <td><?= h($fornecedore->responsavel) ?></td>
             <td><?= h($fornecedore->endereco) ?></td>
             <td><?= h($fornecedore->email) ?></td>
             <td><?= h($fornecedore->telefone) ?></td>
-            
+
             <td><?= ($fornecedore->is_pendente) ? __('Sim') : __('Não') ?></td>
             <td class="actions">
-              <?= $this->Html->link(__('Visualizar'), ['action' => 'view', $fornecedore->id_fornecedor], ['class'=>'btn btn-xs btn-outline-primary', 'escape'=>false]) ?>
-              <?= $this->Html->link(__('Editar'), ['action' => 'edit', $fornecedore->id_fornecedor], ['class'=>'btn btn-xs btn-outline-primary', 'escape'=>false]) ?>
+              <?= $this->Html->link(__('Visualizar'), ['action' => 'view', $fornecedore->id_fornecedor], ['class'=>'btn btn-xs btn-outline-info', 'escape'=>false]) ?>
+              <?= $this->Html->link(__('Editar'), ['action' => 'edit', $fornecedore->id_fornecedor], ['class'=>'btn btn-xs btn-outline-success', 'escape'=>false]) ?>
               <?= $this->Form->postLink(__('Deletar'), ['action' => 'delete', $fornecedore->id_fornecedor], ['class'=>'btn btn-xs btn-outline-danger', 'escape'=>false, 'confirm' => __('Você quer mesmo deletar o fonecedor {0}?', $fornecedore->nome)]) ?>
             </td>
           </tr>
@@ -65,12 +62,10 @@
   </div>
   <!-- /.card-body -->
 
-  <div class="card-footer d-md-flex paginator">
+  <div class="card-footer d-md-flex paginator" style="color: green;">
     <div class="mr-auto" style="font-size:.8rem">
       <?= $this->Paginator->counter(__('Pagina {{page}} de {{pages}}, mostrando {{current}} Fornecedores de {{count}} no total')) ?>
     </div>
-    </div>
-
     <ul class="pagination pagination-sm">
       <?= $this->Paginator->first('<i class="fas fa-angle-double-left"></i>', ['escape'=>false]) ?>
       <?= $this->Paginator->prev('<i class="fas fa-angle-left"></i>', ['escape'=>false]) ?>
@@ -82,3 +77,4 @@
   </div>
   <!-- /.card-footer -->
 </div>
+          </div>
