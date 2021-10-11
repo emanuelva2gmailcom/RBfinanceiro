1cc0a671e0b5fc4a76b8e03413bfb26599b984d9
vinny0240 <88400655+vinny0240@users.noreply.github.com>
2021-10-11 09:35:49 -0300

    Update edit.php

--- templates/Lancamentos/edit.php
+++ templates/Lancamentos/edit.php
@@ -6,14 +6,50 @@
  */
 ?>
 
+<style>
+
+    .btn{
+
+        background-color: green;
+
+        color: white;
+
+        border: 1px solid green;
+
+    }
+
+    .btn:hover{
+
+        background-color: white;
+
+        color: green;
+
+        border: 1px solid green;
+
+    }
+
+    .btn:focus{
+
+        background-color: green;
+
+        color: white;
+
+        border: 1px solid green;
+
+    }
+
+</style>
+
 <?php $this->assign('title', __('Editar Lançamento')); ?>
 
 
 
 
-<div class="card card-primary card-outline">
+<div class="container p-5" style="width: 60%;min-width:80%;border-radius: 20px;">
+
+<div class="card bg-info" style="border-radius: 20px;">
   <?= $this->Form->create($lancamento) ?>
-  <div class="card-body">
+  <div class="card-body text-white">
     <?php
     echo $this->Form->label('Tipo');
     echo $this->Form->select('tipo', ['PREVISTO' => 'PREVISTO', 'REALIZADO' => 'REALIZADO'], ['class' => 'form-control']);
@@ -30,11 +66,19 @@
     ?>
   </div>
 
-  <div class="card-footer d-flex">
-    <div class="ml-auto">
-      <?= $this->Form->button(__('Salvar')) ?>
-      <?= $this->Html->link(__('Cancelar'), ['action' => 'index'], ['class' => 'btn btn-default']) ?>
+  <div class="card-footer bg-white d-flex justify-content-between"  style="border-radius: 0px 0px 20px 20px;">
+
+    <div class="d-flex">
+      <?= $this->Html->link(__('Cancelar'), ['action'=>'index'], ['class'=>'btn btn-default']) ?>
+
     </div>
+
+    <div class="d-flex" style="margin-left: 87%;">
+
+        <?= $this->Form->button(__('Salvar')) ?>
+
+    </div>
+
   </div>
 
   <?= $this->Form->end() ?>
