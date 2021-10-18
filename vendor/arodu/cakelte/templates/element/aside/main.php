d719da4ad6ffe8d1371a76f9704d95bc2e30cb76
vinny0240 <vinnyandrade24@gmail.com>
2021-10-18 10:08:29 -0300

    check ok

--- vendor/arodu/cakelte/templates/element/aside/main.php
+++ vendor/arodu/cakelte/templates/element/aside/main.php
@@ -1,8 +1,20 @@
-<div class="p-3" >
+<style>
+  .tes a{
+    margin-left: 2%;
+  }
+
+  .tes li{
+    margin-bottom: 5%;
+  }
+</style>
+
+<div class="tes p-3" >
   <li class="nav-item d-sm-block" style="list-style: none; color: yellow;">
-    <?= $this->Html->link(__('Configurações'), ['controller' => 'users','action' => 'dashboard', 'plugin' => 'Usermgmt'], ['class' => 'nav-link']) ?>
+    <i class="nav-icon fas fa-tools" style="color: #59CBFF;"></i>
+    <?= $this->Html->link(__('Configurações'), ['controller' => 'users','action' => 'dashboard', 'plugin' => 'Usermgmt']) ?>
   </li>
   <li class="nav-item d-sm-block" style="list-style: none; color: yellow;">
-    <?= $this->Html->link(__('Sair'), ['controller' => 'users','action' => 'logout', 'plugin' => 'Usermgmt'], ['class' => 'nav-link']) ?>
+    <i class="nav-icon fas fa-user-times" style="color: #59CBFF;"></i>
+    <?= $this->Html->link(__('Sair'), ['controller' => 'users','action' => 'logout', 'plugin' => 'Usermgmt']) ?>
   </li>
 </div>
