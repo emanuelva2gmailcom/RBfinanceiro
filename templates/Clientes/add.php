dbaf23823f4921a7c92cfa4baf4eed63179596b9
vinny0240 <vinnyandrade24@gmail.com>
2021-10-21 11:02:42 -0300

    adds responsivos (-lanc)

--- templates/Clientes/add.php
+++ templates/Clientes/add.php
@@ -30,35 +30,30 @@
 
 <?php $this->assign('title', __('Adicionar Cliente') ); ?>
 
-<div class="container p-5" style="width: 60%;min-width:80%;border-radius: 20px;">
-<div class="card bg-info" style="border-radius: 20px;">
+<div class="container d-flex justify-content-center">
+
+  <div class="card card-danger m-5" style="border-radius: 20px; width: 80%">
+    <div class="card-body bg-info" style="border-radius: 20px 20px 0px 0px ;">
   <?= $this->Form->create($cliente) ?>
-  <div class="card-body text-white">
-    <?php
-      echo $this->Form->control('nome');
-      echo $this->Form->control('cpf');
-      echo $this->Form->control('endereco');
-      echo $this->Form->control('email');
-      echo $this->Form->control('telefone');
-      echo $this->Form->control('is_pendente', ['custom' => true]);
-    ?>
+
+    <?= $this->Form->control('nome', ['class' => 'form-control']); ?>
+    <?= $this->Form->control('cpf',  ['class' => 'form-control']); ?>
+    <?= $this->Form->control('endereco',  ['class' => 'form-control']); ?>
+    <?= $this->Form->control('email',  ['class' => 'form-control']); ?>
+    <?= $this->Form->control('telefone',  ['class' => 'form-control']); ?>
+    <?= $this->Form->control('is_pendente', ['custom' => true],  ['class' => 'form-control']); ?>
+
   </div>
 
-  <div class="card-footer bg-white d-flex"  style="border-radius: 0px 0px 20px 20px;">
-
-<div class="mr-auto p-2">
-
-    <?= $this->Html->link(__('Cancelar'), ['action'=>'index'], ['class'=>'btn btn-default']) ?>
-
-</div>
-
-<div class="p-2" >
-
-    <?= $this->Form->button(__('Salvar')) ?>
-
-</div>
-
-</div>
+  <div class="card-footer bg-white d-flex" style="border-radius: 0px 0px 20px 20px;">
+      <div class="mr-auto p-2">
+        <?= $this->Html->link(__('Cancelar'), ['action' => 'index'], ['class' => 'btn btn-default']) ?>
+      </div>
+      <div class="p-2">
+        <?= $this->Form->button(__('Salvar')) ?>
+      </div>
+    </div>
 
   <?= $this->Form->end() ?>
+</div>
 </div>
