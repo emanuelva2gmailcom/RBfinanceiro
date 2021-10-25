c28c2b35914352af1098905939be1320b14f9f42
vinny0240 <vinnyandrade24@gmail.com>
2021-10-25 09:32:37 -0300

    respon 1

--- templates/Lancamentos/renovar.php
+++ templates/Lancamentos/renovar.php
@@ -43,33 +43,27 @@
 </style>
 
 
-<div class="container p-5" style="width: 60%;min-width:80%;border-radius: 20px;">
+<div class="container d-flex justify-content-center">
 
-<div class="card bg-info" style="border-radius: 20px;">
+<div class="card card-danger m-5" style="border-radius: 20px; width: 80%">
+    <div class="card-body bg-info" style="border-radius: 20px 20px 0px 0px ;">
     <?= $this->Form->create($lancamento) ?>
-     <div class="card-body text-white">
-        <?php
-        echo $this->Form->control('valor');
-        echo $this->Form->control('descricao');
-        echo $this->Form->control('data_vencimento');
-        ?>
-    </div>
 
-    <div class="card-footer bg-white d-flex justify-content-between"  style="border-radius: 0px 0px 20px 20px;">
-
-    <div class="d-flex">
-
-        <?= $this->Html->link(__('Cancelar'), ['action'=>'index'], ['class'=>'btn btn-default']) ?>
+        <?= $this->Form->control('valor', ['class' => 'form-control']); ?>
+        <?= $this->Form->control('descricao', ['class' => 'form-control']); ?>
+        <?= $this->Form->control('data_vencimento', ['class' => 'form-control']);?>
 
     </div>
 
-    <div class="d-flex" style="margin-left: 87%;">
-
+    <div class="card-footer bg-white d-flex" style="border-radius: 0px 0px 20px 20px;">
+      <div class="mr-auto p-2">
+        <?= $this->Html->link(__('Cancelar'), ['action' => 'index'], ['class' => 'btn btn-default']) ?>
+      </div>
+      <div class="p-2">
         <?= $this->Form->button(__('Salvar')) ?>
-
+      </div>
     </div>
-
-  </div>
 
     <?= $this->Form->end() ?>
+</div>
 </div>
