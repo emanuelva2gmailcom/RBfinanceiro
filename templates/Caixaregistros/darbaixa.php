ab612d4c0ef644d00ba804aa22d894a3b0e3aaa7
vinny0240 <vinnyandrade24@gmail.com>
2021-11-11 12:18:42 -0300

    agora sim check

--- templates/Caixaregistros/darbaixa.php
+++ templates/Caixaregistros/darbaixa.php
@@ -1,8 +1,8 @@
 
 <div class="container d-flex justify-content-center">
 
-  <div class="carADD card card-danger m-5">
-    <div class="carbADD card-body bg-info">
+  <div class="cardADD card card-danger m-5">
+    <div class="cardbodyADD card-body">
   <?= $this->Form->create($caixaregistro,['type' => 'file']) ?>
 
     <?= $this->Form->control('tipopagamento_id', ['label' => 'Tipo de Pagamento'], ['options' => $tipopagamentos], ['class' => 'form-control']); ?>
@@ -12,7 +12,7 @@
     </div>
   </div>
 
-  <div class="carfADD card-footer bg-white d-flex">
+  <div class="cardfooterADD card-footer d-flex">
       <div class="mr-auto p-2">
         <?= $this->Html->link(__('Cancelar'), ['controller' => 'Lancamentos','action' => 'index'], ['class' => 'btn btnADD btn-default']) ?>
       </div>
