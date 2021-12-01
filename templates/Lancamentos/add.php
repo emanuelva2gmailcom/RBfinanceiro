4abdcd2c7ac65b397438aadf98e3cec02b947c95
Otacilio Filho <trabalhocaiomatheus@gmail.com>
2021-12-01 11:58:24 -0300

    teste

--- templates/Lancamentos/add.php
+++ templates/Lancamentos/add.php
@@ -198,51 +198,51 @@
 ?>
 
 <script>
-    $('document').ready(function() {
-        // evento de "submit"
-        $("#testee ").click(function(e) {
-            // parar o envio para que possamos faze-lo manualmente.
-            e.preventDefault();
-            // captura o formulário
-            var form = $('.tipo');
-            console.log('tesatata')
-            // cria um FormData {Object}
-            var data = new FormData(form);
-            // processar
-            $.ajax({
-                type: "POST",
-                url: "/Lancamentos/add", //acerte o caminho para seu script php
-                data: data,
-                processData: false, // impedir que o jQuery tranforma a "data" em querystring
-                contentType: false, // desabilitar o cabeçalho "Content-Type"
-                //cache: false, // desabilitar o "cache"
-                // manipular o sucesso da requisição
-            }).done(function(retorno) {
-                console.log(retorno);
-                retorno = parseInt(retorno);
+    // $('document').ready(function() {
+    //     // evento de "submit"
+    //     $("#testee ").click(function(e) {
+    //         // parar o envio para que possamos faze-lo manualmente.
+    //         e.preventDefault();
+    //         // captura o formulário
+    //         var form = $('.tipo');
+    //         console.log('tesatata')
+    //         // cria um FormData {Object}
+    //         var data = new FormData(form);
+    //         // processar
+    //         $.ajax({
+    //             type: "POST",
+    //             url: "/Lancamentos/add", //acerte o caminho para seu script php
+    //             data: data,
+    //             processData: false, // impedir que o jQuery tranforma a "data" em querystring
+    //             contentType: false, // desabilitar o cabeçalho "Content-Type"
+    //             //cache: false, // desabilitar o "cache"
+    //             // manipular o sucesso da requisição
+    //         }).done(function(retorno) {
+    //             console.log(retorno);
+    //             retorno = parseInt(retorno);
 
 
-                if (retorno == 1) {
-                    //alert("Formulário enviado com sucesso");
-                    alert('teste')
-                    $("#alertas").modal('show');
-                    console.log();
-                    $(':input', '#caixa')
-                        .not(':button, :submit, :reset, :hidden')
-                        .val('')
-                        .removeAttr('checked')
-                        .removeAttr('selected');
-                } else {
-                    //alert("erro ao enviar formulário");
-                    $("#myModalError").modal('show');
+    //             if (retorno == 1) {
+    //                 //alert("Formulário enviado com sucesso");
+    //                 alert('teste')
+    //                 $("#alertas").modal('show');
+    //                 console.log();
+    //                 $(':input', '#caixa')
+    //                     .not(':button, :submit, :reset, :hidden')
+    //                     .val('')
+    //                     .removeAttr('checked')
+    //                     .removeAttr('selected');
+    //             } else {
+    //                 //alert("erro ao enviar formulário");
+    //                 $("#myModalError").modal('show');
 
 
-                }
+    //             }
 
 
-            });
-        });
-    });
+    //         });
+    //     });
+    // });
 
     // $("#testee").on('submit', function() {
     //     event.preventDefault()
